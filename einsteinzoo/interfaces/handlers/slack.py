d94f4f6c18d6dcf90beccd0a56f652da16564a3f
hironobu koura <hironobu@binaryquaver.com>
2020-03-15 12:09:04 +0900

    join and hello

--- einsteinzoo/interfaces/handlers/slack.py
+++ einsteinzoo/interfaces/handlers/slack.py
@@ -10,7 +10,7 @@ ACCESS_URL = 'https://slack.com/api/oauth.v2.access'
 
 @blueprint.route('/authorize', methods=['GET'])
 def slack_authorize():
-    scope = 'team:read,users:read'
+    scope = 'team:read,users:read,channels:join,chat:write'
     client_id = os.environ['SLACK_CLIENT_ID']
     redirect_uri = os.environ['SLACK_REDIRECT_URI']
     return redirect(AUTHORIZE_URL + urllib.parse.urlencode({ 'scope': scope, 'client_id': client_id, 'redirect_uri': redirect_uri }))
@@ -25,5 +25,13 @@ def slack_access_redirect():
     
     client = slack.WebClient(token="")
     response = client.oauth_v2_access(client_id=client_id, client_secret=client_secret, code=code)
-    print(response['access_token'])
+    os.environ['SLACK_BOT_ACCESS_TOKEN'] = response['access_token']
+
+    client = slack.WebClient(token=os.environ['SLACK_BOT_ACCESS_TOKEN'])
+    response = client.channels_join(name='#general')
+    print(response)
+
+    response = client.chat_postMessage(channel='#general', text='hello')
+    print(response)
+
     return jsonify({'status': 'ok'})
